48e3deef84c8d235b02c3dc155ce4708949ff422
Yangsibai <yangsibai@gmail.com>
2016-04-23 18:39:21 +0800

    update pages

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 func main() {
 	go fetchDomains()
 
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
 	http.HandleFunc("/", homeHanlder)
 	http.HandleFunc("/watch/", watcherHandler)
+
 	http.ListenAndServe(":9024", nil)
 }
